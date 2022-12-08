7fe9bceb6906b2a0a0d5f96ef7c8f4a13ec46b45
Alireza Pazirandeh <maclir@gmail.com>
2022-12-08 11:25:39 +0100

    Add day 6 solution

---
+++ src/main/kotlin/y2022/d6/Main.kt
@@ -0,0 +1,15 @@
+package y2022.d6
+
+import java.io.File
+
+fun main() {
+    val inputL = File("src/main/kotlin/y2022/d6/Input.txt").readText(Charsets.UTF_8).split("").toMutableList()
+    inputL.removeFirst()
+    for (i in 13..inputL.size-1) {
+        val nIn = inputL.subList(i - 13, i + 1)
+        if (nIn.distinct().size == 14) {
+            println(i + 1)
+            break
+        }
+    }
+}
